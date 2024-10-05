3c442f7fc59f1ddc9a4982790dcd94d38661fde9
Tophercwill <45521537+Tophercwill@users.noreply.github.com>
2024-10-05 15:45:21 -0400

    Tree Tiles Implemented

--- Assets/Scripts/tilemap.gd
+++ Assets/Scripts/tilemap.gd
@@ -1,3 +1,4 @@
+class_name WorldMapLayer
 extends TileMapLayer
 
 @export var tile_size: Vector2
@@ -28,17 +29,28 @@ func _unhandled_input(_event: InputEvent) -> void:
 		#If we haven't initialized a tile there, don't place a new one
 		if cell_id == -1:
 			return
-		#Placeholder for cell coordinates until we set up tile visualizations
-		set_cell(coords, selected_tile_id, Vector2i(2, 1))
-		id_map[get_array_index(coords)] = selected_tile_id
-		print("Placing tile id ", selected_tile_id, " at ", coords)
+		place_tile(coords, selected_tile_id)
 		is_placing_tile = false
 		selected_tile_id = -1
 		tile_placed.emit()
 
+
 func select_tile(tile_id: int) -> void:
 	selected_tile_id = tile_id
 	is_placing_tile = true
+
+
+func place_tile(coords: Vector2i, map_id: int = -1, sprite_id: int = -1):
+	if map_id != -1:
+		set_cell(coords, map_id, Vector2i(2, 1))
+		id_map[get_array_index(coords)] = selected_tile_id
+		print("Placing tile id ", selected_tile_id, " at ", coords)
+		
+	if sprite_id != -1:
+		spritelayer.set_cell(coords, sprite_id, Vector2i(0, 0))
+		sprite_id_map[get_array_index(coords)] = sprite_id
+		print("Placing Sprite id ", sprite_id, " at ", coords)
+
 
 #Returns [TileMapID, SpriteLayerID]
 func get_tile_info(coords: Vector2i) -> Array[int]:
@@ -61,6 +73,17 @@ func get_coord_from_position(location: Vector2) -> Vector2i:
 
 func get_array_index(coords: Vector2i) -> int:
 	return coords.x + coords.y * map_size.x
+	
+func get_adjacent_tiles(coords: Vector2i) -> Array[Vector2i]:
+	var output_array: Array[Vector2i] = []
+	for test_vector in [Vector2i(-1, 0), Vector2i(1, 0), Vector2i(0, -1), Vector2i(0, 1)]:
+		test_vector = test_vector + coords
+		if test_vector == coords: continue
+		if test_vector.y < 0 or test_vector.y >= map_size.y: continue
+		if test_vector.x < 0 or test_vector.x >= map_size.x: continue
+		if get_tile_info(test_vector)[0] == -1: 			continue
+		output_array.append(test_vector)
+	return output_array
 
 #No pathfinding, strict nearest
 func get_nearest_tile(starting_location: Vector2, id: int = -1, sprite_id: int = -1) -> Vector2i:
