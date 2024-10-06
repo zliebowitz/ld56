e10f93552f42b5ffcc6d4425f1848039fcc8b7ff
Josh Luoni <josh.luoni@gmail.com>
2024-10-06 19:57:29 -0400

    added code to population hud

--- Assets/Scripts/population_hud.gd
+++ Assets/Scripts/population_hud.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
 
-
+@export var squirrels: Node
+@export var hawks: Node
+@export var fox: Node
+@export var bear: Node
+@export var rabbit: Node
+@export var alligator: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +14,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$GridContainer/AlligatorPop.text = str(alligator.get_child_count())
+	$GridContainer/SquirrelPop.text = str(squirrels.get_child_count())
+	$GridContainer/FoxPop.text = str(fox.get_child_count())
+	$GridContainer/BearPop.text = str(bear.get_child_count())
+	$GridContainer/RabbitPop.text = str(rabbit.get_child_count())
+	$GridContainer/HawkPop.text = str(hawks.get_child_count())
