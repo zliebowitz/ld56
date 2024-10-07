b0a6521580aee122634487de06ef0d80bf904495
Zach Liebowitz <zach@Zliebowitz.com>
2024-10-07 12:24:08 -0400

    fox eats nuts

--- Assets/Scripts/Animals/animal_fox.gd
+++ Assets/Scripts/Animals/animal_fox.gd
@@ -17,19 +17,36 @@ func _process_action(delta: float) -> void:
 				advance_state()
 		STATE.SEEK_FOOD:
 			target = get_nearest_creature_list([Squirrel, Rabbit])
+			var nut_position = tilemap.get_nearest_tile_absolute(position, -1, 0)
+			var nut_valid: bool = nut_position.x !=-999 and nut_position.y != -999
+
 			
-			if is_instance_valid(target):
-				destination = target.position
+			if is_instance_valid(target) || nut_valid:
+				if !nut_valid:
+					destination = target.position
+				elif !is_instance_valid(target):
+					destination = nut_position
+				else:
+					if (position - nut_position).length() < (position - target.position).length():
+						destination = nut_position
+						nut_valid = false
+					else:
+						destination = target.position
+						target = null
 			else:
 				# Try to find food.
 				if (destination == Vector2(-1,-1) or (position - destination).length() < 10):
 					var next_position = Vector2(position.x + randf_range(-100, 100), position.y + randf_range(-50, 50))
 					destination = next_position
 			if move_towards_destination(delta):
-				if not is_instance_valid(target):
+				if not is_instance_valid(target) and not nut_valid:
 					return
 				animation.play("Eat")
-				target.kill()
+				if nut_valid:
+					var coords = tilemap.get_coord_from_position(destination)
+					tilemap.clear_sprite(coords)
+				else:
+					target.kill()
 				create_dna()
 				advance_state()
 		STATE.WAIT:
