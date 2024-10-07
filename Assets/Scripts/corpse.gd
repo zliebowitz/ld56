7ab226c4a7b9e151503bae052b3f43013e126025
TravisBender <travisbender11@gmail.com>
2024-10-07 16:51:05 -0400

    Coropses decay after 40 seconds.

--- Assets/Scripts/corpse.gd
+++ Assets/Scripts/corpse.gd
@@ -4,6 +4,17 @@ extends Sprite2D
 @export var small_bones: Texture2D
 @export var large_bones: Texture2D
 
+var decay
+
+func _ready() -> void:
+	decay = 0
+	
+func _process(delta: float) -> void:
+	decay += delta
+	if decay > 40:
+		queue_free()
+		GlobalManager.update_populations()
+
 func set_bones(small_sized: bool = false):
 	if small_sized:
 		texture = small_bones
